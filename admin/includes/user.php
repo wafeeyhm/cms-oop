e9cd15920c7bdc60cc608a81e0171035c79d06d3
wafeeyhm <wafiy@anak.it>
2024-09-21 18:49:36 +0800

    create run query method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -3,18 +3,24 @@
 class User{
 
     public static function find_all_users(){
-        global $database;
+        
+        return self::run_query("SELECT * FROM users");
 
-        $result = $database->query("SELECT * FROM users");
-        return $result;
     }
 
     public static function find_users_by_id($user_id){
-        global $database;
-
-        $result = $database->query("SELECT * FROM users WHERE id=" . $user_id);
+        
+        $result =  self::run_query("SELECT * FROM users WHERE id=$user_id LIMIT 1");
         $found_user = mysqli_fetch_array($result);
         return $found_user;
+
+    }
+
+    public static function run_query($sql){
+        
+        global $database;
+        $result = $database->query($sql);
+        return $result;
     }
 
 }
