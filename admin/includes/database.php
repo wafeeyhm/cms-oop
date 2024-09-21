10e09977bd1514a398cbd727dcf4491037a4de47
wafeeyhm <wafiy@anak.it>
2024-09-21 18:17:22 +0800

    improve database connection class

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -12,33 +12,40 @@ class Database{
 
     public function open_db_connection(){
 
-        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-        if(mysqli_connect_errno()){
-            die("Database connection failed");
+        if ($this->connection->connect_errno) {
+            # code...
+            die("Database connection failed" . $this->connection->connect_error);
         }
 
     }
 
     public function query($sql){
-        $result = mysqli_query($this->connection, $sql);
+        $result = $this->connection->query($sql);
 
         return $result;
     }
 
     private function confirm_query($result){
+
         if (!$result) {
             # code...
-            die("Query failed");
+            die("Query failed" . $this->connection->error);
         }
 
         return $result;
     }
 
     public function escape_string($sql){
-        $escaped = mysqli_real_escape_string($this->connection, $sql);
+
+        $escaped = $this->connection->real_escapae_string($sql);
 
         return $escaped;
+    }
+
+    public function the_insert_id(){
+        return $this->connection->insert_id;
     }
 
 }
