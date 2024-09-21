4c4e6e98a9c3f27628e5081e07bb26601be06f28
wafeeyhm <wafiy@anak.it>
2024-09-22 00:43:34 +0800

    create method to set session

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -10,6 +10,17 @@ class Session{
         session_start();
     }
 
+    private function check_login(){
+        if(isset($_SESSION['user_id'])){
+            $this->user_id = $_SESSION['user_id'];
+            $this->signed_in = true;
+        }
+        else{
+            $this->signed_in = false;
+            unset($this->user_id);
+        }
+    }
+
 }
 
 $session = new Session();
