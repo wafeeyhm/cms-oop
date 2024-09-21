470b28f7be0e564705a2161b01152c51ee130f7e
wafeeyhm <wafiy@anak.it>
2024-09-21 17:29:19 +0800

    auto establish database connection using constructor

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -4,7 +4,11 @@ require_once("new_config.php");
 
 class Database{
 
-    private $connection;
+    public $connection;
+
+    function __construct(){
+        $this->open_db_connection();
+    }
 
     public function open_db_connection(){
 
@@ -19,6 +23,5 @@ class Database{
 }
 
 $database = new Database();
-$database->open_db_connection();
 
 ?>
