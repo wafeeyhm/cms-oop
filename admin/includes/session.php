f4cae647d1f84dcc2f5a5c65a55c587d46f5b337
wafeeyhm <wafiy@anak.it>
2024-09-22 00:55:52 +0800

    create logout method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -23,6 +23,12 @@ class Session{
         }
     }
 
+    public function logout(){
+        unset($_SESSION['user_id']);
+        unset($this->user_id);
+        $this->signed_in = false;
+    }
+
     private function check_login(){
         if(isset($_SESSION['user_id'])){
             $this->user_id = $_SESSION['user_id'];
