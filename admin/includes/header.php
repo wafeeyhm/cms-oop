eb653c79729b8d7fb6e42f71e58f32d5dc689353
wafeeyhm <wafiy@anak.it>
2024-09-21 17:23:58 +0800

    create database class

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,3 +1,9 @@
+<?php
+
+    ob_start();
+    include("init.php");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
--- admin/includes/new_config.php
+++ admin/includes/new_config.php
@@ -7,10 +7,4 @@ define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','gallery_db');
 
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-if($connection){
-    echo "true";
-}
-
 ?>
