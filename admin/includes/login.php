873a0b2d060b2750b3c26969375f02fa7669d266
wafeeyhm <wafiy@anak.it>
2024-09-22 18:35:59 +0800

    prepare login function

---
+++ admin/includes/login.php
@@ -0,0 +1,32 @@
+<?php
+
+    require_once("init.php");
+
+    if($session->is_signed_in()){
+        redirect("index.php");
+    }
+
+    if(isset($_POST['submit'])){
+
+        $username = trim($_POST['username']); 
+        $password = trim($_POST['password']); 
+
+        //method to check database user
+
+        if($user_found){
+            $session->login($user_found);
+            redirect("index.php");
+        }
+        else{
+            $message = "Incorrect username or password";
+        }
+
+    }
+    else{
+
+        $username = "";
+        $password = "";
+        
+    }
+
+?>
